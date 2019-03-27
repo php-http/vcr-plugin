76fab383e7b0f4c4eae06594139bde7389971b92
Gary PEGEOT <garypegeot@gmail.com>
2019-03-27 22:26:02 +0000

    Optionnaly throw on recorder miss

--- src/ReplayPlugin.php
+++ src/ReplayPlugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Http\Client\Plugin\Vcr;
 
 use Http\Client\Common\Plugin;
+use Http\Client\Exception\RequestException;
 use Http\Client\Plugin\Vcr\NamingStrategy\NamingStrategyInterface;
 use Http\Client\Plugin\Vcr\Recorder\PlayerInterface;
 use Http\Promise\FulfilledPromise;
@@ -25,10 +26,18 @@ final class ReplayPlugin implements Plugin
      */
     private $player;
 
-    public function __construct(NamingStrategyInterface $namingStrategy, PlayerInterface $player)
+    /**
+     * Throw an exception if not able to replay a request.
+     *
+     * @var bool
+     */
+    private $throw;
+
+    public function __construct(NamingStrategyInterface $namingStrategy, PlayerInterface $player, bool $throw = true)
     {
         $this->namingStrategy = $namingStrategy;
         $this->player = $player;
+        $this->throw = $throw;
     }
 
     /**
@@ -42,6 +51,22 @@ final class ReplayPlugin implements Plugin
             return new FulfilledPromise($response->withAddedHeader(static::HEADER_NAME, $name));
         }
 
+        if ($this->throw) {
+            throw new RequestException("Unable to find a response to replay request \"$name\".", $request);
+        }
+
         return $next($request);
+    }
+
+    /**
+     * Whenever the plugin should throw an exception when not able to replay a request.
+     *
+     * @return $this
+     */
+    public function throwOnNotFound(bool $throw)
+    {
+        $this->throw = $throw;
+
+        return $this;
     }
 }
--- tests/ReplayPluginTest.php
+++ tests/ReplayPluginTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Http\Client\Plugin\Vcr\Tests;
 
 use GuzzleHttp\Psr7\Response;
+use Http\Client\Exception\RequestException;
 use Http\Client\Plugin\Vcr\ReplayPlugin;
 use Http\Promise\FulfilledPromise;
 use Http\Promise\Promise;
@@ -17,6 +18,7 @@ class ReplayPluginTest extends AbstractPluginTestCase
 {
     public function testHandleRequest(): void
     {
+        $this->plugin->throwOnNotFound(false);
         $this->namingStrategy->method('name')->willReturn('foo');
         $next = function (): Promise {
             return new FulfilledPromise(new Response(200, [], 'not replayed'));
@@ -36,6 +38,15 @@ class ReplayPluginTest extends AbstractPluginTestCase
                 $this->assertSame(['foo'], $response->getHeader(ReplayPlugin::HEADER_NAME));
                 $this->assertSame('Replayed', (string) $response->getBody());
             });
+    }
+
+    public function testHandleRequestThrow(): void
+    {
+        $this->expectException(RequestException::class);
+        $this->expectExceptionMessage('Unable to find a response to replay request "foo".');
+        $this->namingStrategy->method('name')->willReturn('foo');
+
+        $this->plugin->handleRequest($this->getRequest(), $this->failCallback(), $this->failCallback());
     }
 
     protected function getPluginClass(): string
